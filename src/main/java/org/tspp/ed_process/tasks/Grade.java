5b342cdf6660ba20c1ff9eef57719ef7d0eb703f
Indigowar <indigowar.github@gmail.com>
2023-06-30 15:19:41 +0300

    added implementation for Grade class

--- src/main/java/org/tspp/ed_process/tasks/Grade.java
+++ src/main/java/org/tspp/ed_process/tasks/Grade.java
@@ -4,19 +4,29 @@ import java.util.Objects;
 
 public class Grade {
 
-    public Grade(int value) {
+
+    public Grade(int value) throws IllegalArgumentException {
+        if (value < 0 || value > 100) {
+            throw new IllegalArgumentException("grade value should be between 0 and 100");
+        }
+        this.value = value;
     }
 
     public final int get() {
-        return 0;
+        return this.value;
     }
 
     public final boolean isSatisfactory() {
-        return false;
+        return this.value >= 60;
     }
 
     @Override
-    public boolean equals(Objects obj) {
-        return false;
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Grade g)) return false;
+        return this.value == g.get();
     }
+
+
+    private final int value;
 }
