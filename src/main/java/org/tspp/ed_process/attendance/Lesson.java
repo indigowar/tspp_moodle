60d2cbc7ddee7b24c59952c9e9eedd77ef90babc
Indigowar <indigowar.github@gmail.com>
2023-06-30 13:43:02 +0300

    added implementation for Lesson class

--- src/main/java/org/tspp/ed_process/attendance/Lesson.java
+++ src/main/java/org/tspp/ed_process/attendance/Lesson.java
@@ -5,44 +5,68 @@ import org.tspp.users.Student;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
-public final class Lesson {
+public class Lesson {
     public enum Type {
         LABORATORY, PRACTICAL, LECTURE
     }
 
     public Lesson(Subject s, Type t, String n, LocalDateTime time) {
+        this.subject = s;
+        this.type = t;
+        this.title = n;
+        this.time = time;
+
+        this.marks = new ArrayList<>();
     }
 
     public final Type getType() {
-        return null;
+        return this.type;
     }
 
     public final String getTitle() {
-        return "";
+        return this.title;
     }
 
     public final Subject getSubject() {
-        return null;
+        return this.subject;
     }
 
     public final LocalDateTime getTime() {
-        return null;
+        return this.time;
     }
 
     public boolean hasMark(Student s) {
-        return false;
+        return marks.stream().anyMatch(i -> Objects.equals(i.getStudent(), s));
     }
 
     public final Mark getStudentMark(Student s) throws RuntimeException {
-        return null;
+        var result = marks.stream().filter(i -> Objects.equals(i.getStudent(), s)).findFirst();
+        if (result.isEmpty()) {
+            throw new RuntimeException("student has no mark in that lesson");
+        }
+        return result.get();
     }
 
 
-    void setAttendance(Student s, Mark.Value value) throws RuntimeException {
+    public void setAttendance(Student s, Mark.Value value) throws RuntimeException {
+        if (hasMark(s)) {
+            throw new RuntimeException("student already has a mark for this lesson");
+        }
+        marks.add(new Mark(s, value));
     }
 
     public final ArrayList<Mark> getMarks() {
-        return new ArrayList<>();
+        return marks;
     }
+
+
+    private final Type type;
+    private final String title;
+    private final ArrayList<Mark> marks;
+
+    private final Subject subject;
+    private final LocalDateTime time;
+
 }
