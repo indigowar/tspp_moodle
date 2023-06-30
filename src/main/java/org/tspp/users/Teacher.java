3aaf2d9649b1df3e86e477841dbd05edc9594257
Indigowar <indigowar.github@gmail.com>
2023-06-30 12:37:51 +0300

    added empty class for teacher

---
+++ src/main/java/org/tspp/users/Teacher.java
@@ -0,0 +1,8 @@
+package org.tspp.users;
+
+public class Teacher extends User {
+
+    Teacher(String name, String patronymic, String surname, String mail, String password) {
+        super(name, patronymic, surname, mail, password);
+    }
+}
