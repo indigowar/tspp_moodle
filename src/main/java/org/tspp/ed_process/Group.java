f5430dd6ab4aa2e5289256638fc1fa2817b17f02
Indigowar <indigowar.github@gmail.com>
2023-06-30 12:56:45 +0300

    added implementation for Group class

--- src/main/java/org/tspp/ed_process/Group.java
+++ src/main/java/org/tspp/ed_process/Group.java
@@ -2,27 +2,52 @@ package org.tspp.ed_process;
 
 import org.tspp.users.Student;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public final class Group {
 
     public Group(String name) {
+        this.name = name;
+        this.students = new ArrayList<>();
     }
 
     public String getName() {
-        return "";
+        return this.name;
     }
 
     public boolean hasStudent(Student s) {
-        return false;
+        return students.stream().anyMatch(i -> Objects.equals(i, s));
     }
 
     public void addStudent(Student s) throws RuntimeException {
+        if (hasStudent(s)) {
+            throw new RuntimeException("student is in the group already");
+        }
+        students.add(s);
     }
 
     public void removeStudent(Student s) throws RuntimeException {
+        // if the value was found in the array, has_value =  true
+        boolean has_value = students.remove(s);
+
+        if (!has_value) {
+            throw new RuntimeException("student was not found in the group");
+        }
     }
 
     @Override
     public boolean equals(Object obj) {
-        return false;
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Group g)) {
+            return false;
+        }
+        return Objects.equals(getName(), g.getName());
     }
+
+
+    private final String name;
+    private final ArrayList<Student> students;
 }
