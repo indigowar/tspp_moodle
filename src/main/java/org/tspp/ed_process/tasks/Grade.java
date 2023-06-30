68153f448a3281715f409b963d543b27171e3c85
Indigowar <indigowar.github@gmail.com>
2023-06-30 15:17:07 +0300

    added grade's interface and tests

---
+++ src/main/java/org/tspp/ed_process/tasks/Grade.java
@@ -0,0 +1,22 @@
+package org.tspp.ed_process.tasks;
+
+import java.util.Objects;
+
+public class Grade {
+
+    public Grade(int value) {
+    }
+
+    public final int get() {
+        return 0;
+    }
+
+    public final boolean isSatisfactory() {
+        return false;
+    }
+
+    @Override
+    public boolean equals(Objects obj) {
+        return false;
+    }
+}
