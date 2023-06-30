1bb6dac88d1baff26bb3159639c8c425f5ba4fd8
Indigowar <indigowar.github@gmail.com>
2023-06-30 21:36:53 +0300

    added to Subject class methods for managing tasks and lessons

--- src/main/java/org/tspp/ed_process/Subject.java
+++ src/main/java/org/tspp/ed_process/Subject.java
@@ -1,7 +1,11 @@
 package org.tspp.ed_process;
 
+import org.tspp.ed_process.attendance.Lesson;
+import org.tspp.ed_process.tasks.Requirement;
+import org.tspp.ed_process.tasks.Task;
 import org.tspp.users.Teacher;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -12,6 +16,8 @@ public final class Subject {
         this.teacher = teacher;
 
         this.groups = new ArrayList<>();
+        this.tasks = new ArrayList<>();
+        this.lessons = new ArrayList<>();
     }
 
     public final String getTitle() {
@@ -38,12 +44,31 @@ public final class Subject {
         return groups.stream().anyMatch(i -> Objects.equals(i, g));
     }
 
+    public Task createTask(Teacher teacher, Task.Type type, String name, Requirement req, int limit) throws RuntimeException {
+        if (Objects.equals(this.teacher, teacher)) {
+            throw new SecurityException("this teacher does not have permissions to manage this subject");
+        }
+        var task = new Task(type, name, this, req, limit);
+        tasks.add(task);
+        return task;
+    }
 
-    // TODO: createTask()
-    // TODO: createLesson()
-    // TODO: getTasks()
-    // TODO: getLessons()
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
 
+    public Lesson createLesson(Teacher teacher, Lesson.Type type, String title, LocalDateTime time) {
+        if (Objects.equals(this.teacher, teacher)) {
+            throw new SecurityException("this teacher does not have permissions to manage this subject");
+        }
+        var lesson = new Lesson(this, type, title, time);
+        lessons.add(lesson);
+        return lesson;
+    }
+
+    public ArrayList<Lesson> getLessons() {
+        return lessons;
+    }
 
     private final String title;
     private final Teacher teacher;
@@ -51,4 +76,6 @@ public final class Subject {
     private final ArrayList<Group> groups;
 
 
+    private final ArrayList<Task> tasks;
+    private final ArrayList<Lesson> lessons;
 }
