99342d6eda25d3b8e06fbc523a4dc5fa7136a558
Indigowar <indigowar.github@gmail.com>
2023-06-30 16:06:13 +0300

    fixed bug: Teacher constructor was not public.

--- src/main/java/org/tspp/users/Teacher.java
+++ src/main/java/org/tspp/users/Teacher.java
@@ -2,7 +2,7 @@ package org.tspp.users;
 
 public class Teacher extends User {
 
-    Teacher(String name, String patronymic, String surname, String mail, String password) {
+    public Teacher(String name, String patronymic, String surname, String mail, String password) {
         super(name, patronymic, surname, mail, password);
     }
 }
