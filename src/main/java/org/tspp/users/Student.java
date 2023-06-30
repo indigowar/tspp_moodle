af96c98e7ef85f0ac118b27c179d8f3a996885b1
Indigowar <indigowar.github@gmail.com>
2023-06-30 12:37:11 +0300

    added empty class for student

---
+++ src/main/java/org/tspp/users/Student.java
@@ -0,0 +1,7 @@
+package org.tspp.users;
+
+public class Student extends User {
+    Student(String name, String patronymic, String surname, String mail, String password) {
+        super(name, patronymic, surname, mail, password);
+    }
+}
