cf0e008b222c5c15ba0a93ac5c004eed897d46c7
Indigowar <indigowar.github@gmail.com>
2023-07-01 14:27:52 +0300

    made a AuthService

---
+++ src/main/java/org/tspp/services/AuthService.java
@@ -0,0 +1,37 @@
+package org.tspp.services;
+
+import org.tspp.users.Teacher;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class AuthService {
+
+    public AuthService(Storages storages) {
+        this.storages = storages;
+    }
+
+    public StudentService logInAsStudent(String email, String password) throws RuntimeException {
+        var student = storages.getStudents()
+                .stream()
+                .filter(i -> Objects.equals(email, i.getMail()) && i.validatePassword(password))
+                .findFirst();
+        if (student.isEmpty()) {
+            throw new RuntimeException("given credentials are invalid");
+        }
+        return new StudentService(storages, student.get());
+    }
+
+    public TeacherService logInAsTeacher(String email, String password) throws RuntimeException {
+        var teacher = storages.getTeachers()
+                .stream()
+                .filter(i -> Objects.equals(i.getMail(), email) && i.validatePassword(password))
+                .findFirst();
+        if (teacher.isEmpty()) {
+            throw new RuntimeException("given credentials are invalid");
+        }
+        return new TeacherService(storages, teacher.get());
+    }
+
+    private final Storages storages;
+}
